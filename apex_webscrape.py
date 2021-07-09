c5a5b70beef4b15bdeefc528f46615267e8db494
Christopher Rossi <61894684+DuckyZero@users.noreply.github.com>
2021-07-09 14:54:21 -0500

    Update apex_webscrape.py

--- apex_webscrape.py
+++ apex_webscrape.py
@@ -1,47 +1,49 @@
 import requests
 from bs4 import BeautifulSoup
-import pandas as pd
+
 # Have to pip install lxml to work properly
+# testLink = 'https://www.apexkeyboards.ca/collections/switches/products/alpacas'
 
 baseurl = 'https://www.apexkeyboards.ca/'
 productLink = []
 switchList = []
+tag = ''
+
 
 # for loop for multiple product pages
-#for x in range(1,6):
+# for x in range(1,6):
 
-r = requests.get('https://www.apexkeyboards.ca/collections/switches')
-soup = BeautifulSoup(r.content, 'lxml')
+# print(product_list)
 
-productList = soup.find_all('div', class_='grid-product__wrapper')
-#print(productList)
-
-for item in productList:
-    for link in item.find_all('a', href=True):
-        # if-statement removes duplicate links
-        if (baseurl + link['href']) not in productLink:
-            productLink.append(baseurl + link['href'])
-            #print(link['href'])
-
-
-#testLink = 'https://www.apexkeyboards.ca/collections/switches/products/alpacas'
-
-
-for link in productLink:
-    r = requests.get(link)
+def find_product(tag):
+    r = requests.get('https://www.apexkeyboards.ca/collections/{}'.format(tag))
     soup = BeautifulSoup(r.content, 'lxml')
+    product_list = soup.find_all('div', class_='grid-product__wrapper')
 
-    name = soup.find('h1', class_='product-single__title').text.strip()
-    price = soup.find('span', class_='product-single__price').text.strip()
+    for item in product_list:
+        for link in item.find_all('a', href=True):
+            # if-statement removes duplicate links
+            if (baseurl + link['href']) not in productLink:
+                productLink.append(baseurl + link['href'])
+                # print(link['href'])
 
-    switch = {
-        'name': name,
-        'price': price
-    }
-    #print(switch)
-    switchList.append(switch)
-    print('Saving: ', switch['name'])
+    for link in productLink:
+        r = requests.get(link)
+        soup = BeautifulSoup(r.content, 'lxml')
 
-# Transform our switch dictionary into a data frame
-df = pd.DataFrame(switchList)
-print(df.head(15))
+        name = soup.find('h1', class_='product-single__title').text.strip()
+        price = soup.find('span', class_='product-single__price').text.strip()
+
+        print(name + " " + price)
+
+    productLink.clear()
+
+# Testing multiple products
+# tag = 'lubes'
+# find_product(tag)
+# print("")
+# tag = 'switches'
+# find_product(tag)
+# print("")
+# tag = 'tuning-parts'
+# find_product(tag)
